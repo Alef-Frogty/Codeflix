1aca79e5da7a11d4f7b8b4de3be19bbf49d3bc64
Alef Rocha <alef.santos.rocha2@gmail.com>
2025-09-24 17:32:23 -0400

    Add. uuid na Entidade e categoria
    Problemas em busca de arquivo Value-object pela importação no arquivo de value-object.spec

--- src/category/domain/__tests__/category.entity.ts
+++ src/category/domain/__tests__/category.entity.ts
@@ -1,6 +1,7 @@
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
 
 export type CategoryConstructorProps = {
-    category_id?: string;
+    category_id?: Uuid;
     name: string;
     description?: string| null;
     is_active?: boolean;
@@ -16,7 +17,7 @@ export type CategoryCreateCommand = {
 
 
 export class Category {
-    category_id: string | undefined;
+    category_id: Uuid;
     name: string;
     description: string | null;
     is_active: boolean;
@@ -24,7 +25,7 @@ export class Category {
 
 
 constructor(props: CategoryConstructorProps) {
-this.category_id = props.category_id ?? undefined;
+this.category_id = props.category_id ?? new Uuid();
 this.name = props.name;
 this.description = props.description ?? null;
 this.is_active = props.is_active ?? true;
@@ -53,7 +54,7 @@ deactivate(){
 
 toJSON() {
     return {
-        category_id: this.category_id,
+        category_id: this.category_id.id,
         name: this.name,
         description: this.description,
         is_active: this.is_active,
--- src/category/domain/shared/domain/__tests__/value-object.spec.ts
+++
@@ -1,5 +0,0 @@
-
-
-    describe('ValueObject Unit Tests', () => {
-
-    })
